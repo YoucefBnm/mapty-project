efbd61d975aae9349c9345d69bc5f0db661b8300
Josef Bnm <ssefbnm@gmail.com>
2021-01-02 17:22:28 +0100

    add marker

--- script.js
+++ script.js
@@ -18,6 +18,33 @@ navigator.geolocation.getCurrentPosition(
         const {longitude} = position.coords
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
 
+        const coords = [latitude, longitude]
+        // 'map' is the id in html 
+        // '13' is the level of zoom
+        const map = L.map('map').setView(coords, 5);
+    // change the default look of 'streetmap' by another
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    map.on('click', (mapEvent) => {
+        console.log(mapEvent)
+        const { lat, lng } = mapEvent.latlng
+
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+    })
+
 }, function() {
-    alert('you\'re not in earth!! could not get your position')
+    alert('you\'re not on earth!! could not get your position')
 })
+
