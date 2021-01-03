d71f143003bc4ef6e3ecc272597e0f04188fbc43
Josef Bnm <ssefbnm@gmail.com>
2021-01-03 15:01:58 +0100

    refactor app use OOP

--- script.js
+++ script.js
@@ -11,57 +11,75 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent
+// instead of defining them in global-scope
+// => declare map & mapEvent as private prop in App class
+// let map, mapEvent
+class App {
+    // private
+    #map;
+    #mapEvent
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this))
+        inputType.addEventListener('change', this._toggleElevationField)
+    }
 
-navigator.geolocation.getCurrentPosition(
-    // takes two function, one for success and one for error
-    function(position){
+    _getPosition() {
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+                alert('You\'re not on earth!! Could not get your position')
+            })
+        }        
+    }
+
+    _loadMap(position) {
         const {latitude} = position.coords
         const {longitude} = position.coords
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
-
         const coords = [latitude, longitude]
         // 'map' is the id in html 
         // '13' is the level of zoom
-        map = L.map('map').setView(coords, 5);
-    // change the default look of 'streetmap' by another
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    // handle clicks on map
-    map.on('click', (mapE) => {
-        mapEvent = mapE
+        this.#map = L.map('map').setView(coords, 5);
+        // change the default look of 'streetmap' by another
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+        // handle clicks on map
+        this.#map.on('click', this._showForm.bind(this))
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE
         // show form on click 
         form.classList.remove('hidden')
         inputDistance.focus()
-    
-     })
+    }
 
-}, function() {
-    alert('you\'re not on earth!! could not get your position')
-})
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    // clear input fields
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
-    // display marker
-    const { lat, lng } = mapEvent.latlng
+    _newWorkout(e) {
+        e.preventDefault()
+        // clear input fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+        const { lat, lng } = this.#mapEvent.latlng
+        // display marker
+        L.marker([lat, lng])
+            .addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+                }))
+                .setPopupContent('Workout')
+                .openPopup();
+    }
+}
 
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup'
-            }))
-            .setPopupContent('Workout')
-            .openPopup();
-})
+const app = new App()
 
-inputType.addEventListener('change', () => {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
+
+
